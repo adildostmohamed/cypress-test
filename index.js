3a0f6aa80d25ec09292394e38e8cb7f1d0716495
adildostmohamed@corp.amazonworkspaces.com <adildostmohamed@corp.amazonworkspaces.com>
2019-09-18 17:14:58 +0100

    additional config in index

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const cypress = require('cypress')
 
 cypress.run({
-  spec: './cypress/integration/heap_demo.spec.js'
+  spec: './cypress/integration/heap_demo.spec.js',
+  config: {
+    projectId: '56xjvw',
+    video: false
+  },
+  key: '808cba80-9c99-4a41-bdbf-5953f1406ef7',
+  record: true
 })
 .then((results) => {
   console.log(results)
