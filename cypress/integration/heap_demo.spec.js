382015d7e44d2daa51835e10b03928e885af4f60
Adil <adil_dostmohamed@yahoo.co.uk>
2019-09-20 13:02:53 +0100

    change to 10 iterations to test user id assigned in heap

--- cypress/integration/heap_demo.spec.js
+++ cypress/integration/heap_demo.spec.js
@@ -1,8 +1,8 @@
 describe('Cypress + Heap POC', function() {
-  const appUrl = 'http://localhost:3000/'
+  const appUrl = 'http://localhost:3000/';
   beforeEach(() => {
-    cy.visit(appUrl)
-  })
+    cy.visit(appUrl);
+  });
 
   // it('Artist page', function() {
   //   cy.visit(appUrl)
@@ -20,15 +20,28 @@ describe('Cypress + Heap POC', function() {
   // })
 
   it('Should search multiple times', () => {
-    const loopCount =  500;
-    const searchTerms = ['Max Cooper', 'Erased Tapes', 'BRUK', 'hijmer', 'Safer At Night', 'Max Cooper', 'Erased Tapes', 'BRUK', 'hijmer', 'Safer At Night']
-    for(let i = 0; i < loopCount; i++) {
-      const searchTerm = searchTerms[i]
-      cy.visit(appUrl)
-      cy.get('.btn--search-alt').click()
-      cy.get('.search-form__input').type(searchTerm).type('{enter}')
-      cy.url().should('include', '/search')
-      cy.get('.content-header__title').should('contain', searchTerm)
+    const loopCount = 10;
+    const searchTerms = [
+      'Max Cooper',
+      'Erased Tapes',
+      'BRUK',
+      'hijmer',
+      'Safer At Night',
+      'Max Cooper',
+      'Erased Tapes',
+      'BRUK',
+      'hijmer',
+      'Safer At Night'
+    ];
+    for (let i = 0; i < loopCount; i++) {
+      const searchTerm = searchTerms[i];
+      cy.visit(appUrl);
+      cy.get('.btn--search-alt').click();
+      cy.get('.search-form__input')
+        .type(searchTerm)
+        .type('{enter}');
+      cy.url().should('include', '/search');
+      cy.get('.content-header__title').should('contain', searchTerm);
     }
-  })
-})
+  });
+});
--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const cypress = require('cypress')
+const cypress = require('cypress');
 
-cypress.run({
-  spec: './cypress/integration/heap_demo.spec.js',
-  config: {
-    projectId: '56xjvw',
-    video: false
-  },
-  key: '808cba80-9c99-4a41-bdbf-5953f1406ef7',
-  record: true
-})
-.then((results) => {
-  console.log(results)
-})
-.catch((err) => {
-  console.error(err)
-})
+cypress
+  .run({
+    spec: './cypress/integration/heap_demo.spec.js',
+    config: {
+      projectId: '56xjvw',
+      video: false
+    },
+    key: '808cba80-9c99-4a41-bdbf-5953f1406ef7',
+    record: true
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .catch(err => {
+    console.error(err);
+  });
