51c339de68247039321b9b91aeed1311e14b7d13
Adil <adil_dostmohamed@yahoo.co.uk>
2019-09-26 17:55:25 +0100

    add in node-cron and schedule trigger ever 2 mins

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const cypress = require('cypress');
+const cron = require('node-cron');
 
-cypress
-  .run({
-    spec: './cypress/integration/heap_demo.spec.js',
-    config: {
-      projectId: '56xjvw',
-      video: false
-    },
-    key: '808cba80-9c99-4a41-bdbf-5953f1406ef7',
-    record: true
-  })
-  .then(results => {
-    console.log(results);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+const cypress_trigger = () => {
+  return cypress
+    .run({
+      spec: './cypress/integration/heap_demo.spec.js',
+      config: {
+        projectId: '56xjvw',
+        video: false
+      },
+      key: '808cba80-9c99-4a41-bdbf-5953f1406ef7',
+      record: true
+    })
+    .then(results => {
+      console.log(results);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+cron.schedule('*/2 * * * *', () => {
+  console.log('TIME TO RUN!');
+  cypress_trigger();
+});
