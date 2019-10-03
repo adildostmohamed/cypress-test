13360ff8bbf1395157eb5944967fbd5e8c86902b
Adil <adil_dostmohamed@yahoo.co.uk>
2019-10-03 16:19:56 +0100

    added comments

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 require('dotenv').config();
 const cron = require('node-cron');
+
+// import all the triggers for the different interactions to execute
 const searchSubmissionTrigger = require('./triggers/search_submission_trigger');
 const visitArtistPageTrigger = require('./triggers/visit_artist_page_trigger');
 
-// const searchSubmissionSpec = path.join(
-//   './cypress',
-//   'integration',
-//   'search_submission.spec.js'
-// );
-// const searchSubmissionTestName = 'Search Submission';
-// const searchSubmissionTest = cypress_trigger(
-//   searchSubmissionSpec,
-//   searchSubmissionTestName
-// );
-
-// cron.schedule('*/2 * * * *', () => {
-//   console.log('TIME TO RUN!');
-//   searchSubmissionTrigger();
-//   visitArtistPageTrigger();
-// });
-
-searchSubmissionTrigger();
-visitArtistPageTrigger();
+// determine how often you want the scheduled trigger to run
+cron.schedule('*/2 * * * *', () => {
+  console.log('TIME TO RUN!');
+  searchSubmissionTrigger();
+  visitArtistPageTrigger();
+});
